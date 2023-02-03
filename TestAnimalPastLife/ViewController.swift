82a4c224d5c562c0c407281c605647f27873cb49
KarinaChadaeva <118630641+KarinaChadaeva@users.noreply.github.com>
2023-02-03 15:41:48 +0300

    Initial Commit

---
+++ TestAnimalPastLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAnimalPastLife
+//
+//  Created by Карина Чадаева on 03.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
